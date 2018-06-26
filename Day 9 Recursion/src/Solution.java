5ded5db861824ae1b3a8339efb8ee5b8399969cc
Akshay Adagale <39808339+AkshayAdagaleGitHub@users.noreply.github.com>
2018-06-26 17:54:52 -0400

    Add files via upload

---
+++ Day 9 Recursion/src/Solution.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the factorial function below.
+    static int factorial(int n) {
+    	
+    	if(n==0 || n==1) {
+    		return 1;
+    	}
+    	
+    	return factorial(n-1) * n;
+    	//return fact;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        //scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int result = factorial(n);
+        System.out.println("The Factorial of " + n + "is " + result);
+        /*bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+*/
+        scanner.close();
+    }
+}
