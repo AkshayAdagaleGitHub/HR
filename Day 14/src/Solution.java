1121e5833d05f65ce56e19655d2830e31e0d6fa5
Akshay Adagale <39808339+AkshayAdagaleGitHub@users.noreply.github.com>
2018-07-05 23:15:38 -0400

    Add files via upload

---
+++ Day 14/src/Solution.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
+}
